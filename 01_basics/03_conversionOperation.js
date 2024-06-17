7df8ee688511dbee20ade15e610a1617cd7c2b63
Anuj tiwari <145784442+AnujTiwari27@users.noreply.github.com>
2024-06-17 06:50:41 +0000

    Convesion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+
+//  "33" => 33
+// "33abc" => NaN
+//  true = 1 ; false = 0 
+
+let isLoggedIn = 1
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false ; "Anuj" => true
