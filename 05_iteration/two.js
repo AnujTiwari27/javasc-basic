ba1d85454f19c2ef3897b0c8cebef8febc113080
Anuj tiwari <145784442+AnujTiwari27@users.noreply.github.com>
2024-07-19 14:06:27 +0000

    while and do-while loops

---
+++ 05_iteration/two.js
@@ -0,0 +1,29 @@
+
+let index = 0
+while (index <= 10){
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["flash" , "superman" , "batman" , "spiderman"]
+let ind = 0
+while(ind < myArray.length){
+    // console.log(myArray[ind])
+    ind++
+}
+
+
+// let score = 1 
+// do {
+//     console.log(score)
+//     score = score + 2
+// } while (score <10);
+
+
+
+let score = 11
+
+do {
+    console.log(score)
+    score = score + 2
+} while (score <10);
